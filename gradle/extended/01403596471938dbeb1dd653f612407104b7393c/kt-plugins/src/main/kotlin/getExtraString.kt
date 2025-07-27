4d8f4bd5c90bb459b56e44867dd236daa5361f5e
Kevin Le Perf <codlabtech@gmail.com>
2025-07-27 21:55:15 +0200

    remove unecessary dep

--- gradle/extended/01403596471938dbeb1dd653f612407104b7393c/kt-plugins/src/main/kotlin/getExtraString.kt
+++
@@ -1,6 +0,0 @@
-import org.gradle.api.Project
-import org.gradle.kotlin.dsl.extra
-
-fun Project.getExtraString(name: String, default: String = "") =
-    if (extra.has(name)) extra[name]?.toString()
-    else default
