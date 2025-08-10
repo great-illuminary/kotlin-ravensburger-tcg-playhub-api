264f2c295cde3ec439bd397e425d87250e3a9bad
Kevin Le Perf <codlabtech@gmail.com>
2025-08-10 13:52:15 +0200

    add a simplistic retry

--- kotlin-ravensburger-tcg-playhub-api/src/commonMain/kotlin/eu/codlab/lorcana/rph/utils/Clients.kt
+++ kotlin-ravensburger-tcg-playhub-api/src/commonMain/kotlin/eu/codlab/lorcana/rph/utils/Clients.kt
@@ -6,7 +6,9 @@ import eu.codlab.lorcana.rph.FallbackRequest
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.http.isSuccess
+import kotlinx.coroutines.delay
 import kotlinx.serialization.json.Json
+import kotlin.time.Duration.Companion.seconds
 
 internal class Clients(private val fallbackRequest: FallbackRequest? = null) {
     private val json = Json {
@@ -27,10 +29,16 @@ internal class Clients(private val fallbackRequest: FallbackRequest? = null) {
 
     suspend inline fun <reified T> get(url: String): T {
         // TODO manage issues which are striclty related to the data mapping
-        val answer = client.get(url)
+        var retry = 0
+        while (retry < 3) {
+            val answer = client.get(url)
 
-        if (answer.status.isSuccess()) {
-            return answer.body()
+            if (answer.status.isSuccess()) {
+                return answer.body()
+            }
+
+            retry--
+            delay(retry.seconds)
         }
 
         val result: T? = fallbackRequest?.invoke(url)?.let { request ->
